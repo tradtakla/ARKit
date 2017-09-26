6c31e55aca527f9322f338ac8cf95debfb2d0263
Takla Trad <takla.trad@outlook.com>
2017-09-26 15:26:18 +0300

    initial commit

---
+++ MeasuringTool/MeasuringTool/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  MeasuringTool
+//
+//  Created by Karen Najem on 9/26/17.
+//  Copyright © 2017 NM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
---
+++ MeasuringTool/MeasuringTool/ViewController.m
@@ -0,0 +1,92 @@
+//
+//  ViewController.m
+//  MeasuringTool
+//
+//  Created by Karen Najem on 9/26/17.
+//  Copyright © 2017 NM. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController () <ARSCNViewDelegate>
+
+@property (nonatomic, strong) ARSCNView *sceneView;
+
+@end
+
+    
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.sceneView = [[ARSCNView alloc] initWithFrame:self.view.frame];
+    [self.view addSubview:self.sceneView];
+
+    // Set the view's delegate
+    self.sceneView.delegate = self;
+    
+    // Show statistics such as fps and timing information
+    self.sceneView.showsStatistics = YES;
+    
+    // Create a new scene
+    SCNScene *scene ;
+    [self addCrossSign];
+//
+//    // Set the scene to the viewnd on object of type 'SCNG
+//    self.sceneView.scene = scene;
+}
+
+-(void)addCrossSign{
+    
+    UILabel *label = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 100, 30)];
+    label.text = @"+";
+    label.textAlignment = NSTextAlignmentCenter;
+    label.center = self.sceneView.center;
+    
+    [self.sceneView addSubview:label];
+    
+    
+    
+}
+
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    // Create a session configuration
+    ARWorldTrackingConfiguration *configuration = [ARWorldTrackingConfiguration new];
+
+    // Run the view's session
+    [self.sceneView.session runWithConfiguration:configuration];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    
+    // Pause the view's session
+    [self.sceneView.session pause];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - ARSCNViewDelegate
+
+/*
+// Override to create and configure nodes for anchors added to the view's session.
+- (SCNNode *)renderer:(id<SCNSceneRenderer>)renderer nodeForAnchor:(ARAnchor *)anchor {
+    SCNNode *node = [SCNNode new];
+ 
+    // Add geometry to the node...
+ 
+    return node;
+}
+*/
+
+
+
+@end
